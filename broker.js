e2768e816d99fe222e01ecdd67525cb15bb399cb
zoneix <zoneix@gmail.com>
2016-11-21 17:27:22 -0500

    testing env variables

--- broker.js
+++ broker.js
@@ -7,15 +7,17 @@
 const express = require('express');
 
 
-var config = require('./config');
+//var config = require('./config');
 
 var app = require('express')();
 
 app.get('/', function (req, res) {
-    res.send('Hey man - config file has this port for Redis \n' + config.redis.port);
+    //res.send('Hey man - config file has this port for Redis \n' + config.redis.port);
+    res.send('Hey man - Tropo token \n' + process.env.TROPO_SMS_TOKEN
+    + '\n Redis connection string is \n' + process.env.REDIS_CONNECTOR
+    );
 });
 
-
-var listener = app.listen(8080, function(){
+var listener = app.listen(process.env.BROKER_LISTEN_PORT, function(){
     console.log('Listening on port ' + listener.address().port);
 });
