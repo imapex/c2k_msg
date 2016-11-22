2f33d148fb62833b24c7ad3c97b77495d541bab5
zoneix <zoneix@gmail.com>
2016-11-22 13:24:26 -0500

    updated APP URL

--- broker.js
+++ broker.js
@@ -14,7 +14,8 @@ var app = require('express')();
 app.get('/', function (req, res) {
     //res.send('Hey man - config file has this port for Redis \n' + config.redis.port);
     res.send('Hey man - Tropo token \n' + process.env.TROPO_SMS_TOKEN
-    + '\n Redis connection string is \n' + process.env.REDIS_CONNECTOR
+        + '\n Redis connection string is \n' + process.env.REDIS_CONNECTOR
+        + '\n App URL is \n' + process.env.APP_URL
     );
 });
 
